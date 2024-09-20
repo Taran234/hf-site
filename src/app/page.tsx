66533a74e03137f0333fdf7025adfa1edede2734
Taran234 <ts406093@gmail.com>
2024-09-20 10:45:15 -0400

    analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import DynamicPage from "./_components/dynamic-page";
+import { client } from "./lib/contentful";
 
 
-export default function Home() {
+export default async function Home() {
+  const res = await client.getEntries({ content_type: 'bloghf' });
+  const posts = res.items;
+
+  console.log(posts);
+
   return <DynamicPage keyword="Toronto" />;
 }
